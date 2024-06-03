d53340520c472d427c1808fe14fca1891f182eb3
Bimal Gharti Magar <bimal@endpointdev.com>
2024-06-03 07:17:47 +0545

    update nuxt and prerender logic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,21 @@
 let itemsUrl = {}
 import country from './assets/server/countries.json';
 country.forEach(x => {
-  if (x.name.common.toLowerCase().startsWith('a') 
-    || x.name.common.toLowerCase().startsWith('b') 
-    || x.name.common.toLowerCase().startsWith('c')
-    || x.name.common.toLowerCase().startsWith('d')
-    || x.name.common.toLowerCase().startsWith('e')
-    || x.name.common.toLowerCase().startsWith('f')
-    || x.name.common.toLowerCase().startsWith('g')
-    || x.name.common.toLowerCase().startsWith('h')
-    || x.name.common.toLowerCase().startsWith('i')
-  ) {
+  // if (
+  //   x.name.common.toLowerCase().startsWith('a') 
+  //   || x.name.common.toLowerCase().startsWith('b') 
+  //   || x.name.common.toLowerCase().startsWith('c')
+  //   || x.name.common.toLowerCase().startsWith('d')
+  //   || x.name.common.toLowerCase().startsWith('e')
+  //   || x.name.common.toLowerCase().startsWith('f')
+  //   || x.name.common.toLowerCase().startsWith('g')
+  //   || x.name.common.toLowerCase().startsWith('h')
+  //   || x.name.common.toLowerCase().startsWith('i')
+  //   || x.name.common.toLowerCase().startsWith('j')
+  //   || x.name.common.toLowerCase().startsWith('k')
+  //   || x.name.common.toLowerCase().startsWith('l')
+  //   || x.name.common.toLowerCase().startsWith('m')
+  // ) {
     let key = `/country/${x.name.common}`;
     let isrKey = `/isr/country/${x.name.common}`;
     let swrKey = `/swr/country/${x.name.common}`;
@@ -22,7 +27,7 @@ country.forEach(x => {
       [swrKey]: { swr: true },
       [prerenderKey]: { prerender: true },
     }
-  }
+  // }
 })
 
 
@@ -49,20 +54,21 @@ export default defineNuxtConfig({
         'Button',
         'DataTable',
         'Card',
-        'VirtualScroller'
+        'VirtualScroller',
+        'Paginator'
       ]
     }
   },
   routeRules: {
     '/': { prerender: true },
-    '/country': { ssr: false, prerender: false },
-    '/university': { ssr: false, prerender: false },
-    '/isr/country': { isr: true, prerender: false },
-    '/isr/university': { isr: 60, prerender: false },
-    '/swr/country': { swr: true, prerender: false },
-    '/swr/university': { swr: 60, prerender: false },
-    // '/prerender/country': { prerender: true },
-    // '/prerender/university': { prerender: true },
+    '/country': { ssr: false },
+    '/university': { ssr: false },
+    '/isr/country': { isr: true },
+    '/isr/university': { isr: 60 },
+    '/swr/country': { swr: true },
+    '/swr/university': { swr: 60 },
+    '/prerender/country': { prerender: true },
+    '/prerender/university': { prerender: true },
     ...itemsUrl
   },
   nitro: {
